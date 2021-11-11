54a790f37785a7d050b884e80147a0a7a63a6c5d
dondondooooon <89593997+dondondooooon@users.noreply.github.com>
2021-11-11 10:47:35 +0100

    new tors 11.nov

--- Project4/src/func.cpp
+++ Project4/src/func.cpp
@@ -1,61 +1,29 @@
 #include "func.hpp"
 
 // Constructor Definition
-Lattice::Lattice(double N){
-    S_ = mat(N,N);
+Lattice::Lattice(double N_in, double T_in){
+    S_ = mat(N_in,N_in);
+    Snew_ = = mat(N_in,N_in);
+    L_ = N_in;
+    beta_ = 1./(kb*T_in);
 }
 
-// Generate 1 of 16 possible states of 2x2 System
-mat Lattice::S2(int a, int b, int c, int d){
-    if(a == 0){ 
-        a = -1;
-    }
-    if(b == 0){
-        b = -1;
-    }
-    if(c == 0){
-        c = -1;
-    }
-    if(d == 0){
-        d = -1;
-    }
-    arma::mat A = arma::mat(2,2);
-    A(0,0) = a;
-    A(0,1) = b;
-    A(1,0) = c;
-    A(1,1) = d;
-    return A;
-}
-
-// Generate all 16 possible states of 2x2 System
-void Lattice::S2_all(){
-    for (int a = 0; a<2; a++){
-        for (int b = 0; b<2; b++){
-            for (int c = 0; c<2; c++){
-                for (int d = 0; d<2; d++){
-                    mat S_ = S2(a,b,c,d);
-                    all_S.push_back(S_);
-                }
+// Generate a Random Spin Configuration
+mat Lattice::S_gen(){
+    int randNum;
+    int A(L_,L_);
+    srand(time(NULL)); // Seed Generate
+    for (int i = 0;i< L_; i++){
+        for(int j = 0;j< L_; j++){
+            int randNum = rand() % 2;
+            if(randNum == 0){
+                randNum = -1;
             }
+            A(i,j) = randNum;
         }
     }
-    
+    return A;
 }
-
-// // Generate a Random Spin Configuration
-// mat rand_S(double N, mat A){
-// int randNum;
-// srand(time(NULL));
-//     for (int i = 0;i< N; i++){
-//         for(int j = 0;j< N; j++){
-//             int randNum = rand() % 2;
-//             if(randNum == 0){
-//                 randNum = -1;
-//             }
-//             A(i,j) = randNum;
-//         }
-//     }
-// }
 
 // Periodic Boundary Condition
 int Lattice::Periodic(int i, int L){
@@ -63,13 +31,12 @@ int Lattice::Periodic(int i, int L){
     return correct_index;
 }
 
-// Calculate Total Energy in a Spin Configuration
+// Calculate Total Energy in a given Spin Configuration
 int Lattice::E(mat S){
     int E = 0;
-    int N = S.size();
-    int L = sqrt(N);
-    for (int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
+    int L = sqrt(S.size());
+    for (int i=0; i<L; i++){
+        for(int j=0; j<L; j++){
             int s_elem = S(i,j);
             int up = s_elem * S(Periodic(i-1,L),Periodic(j,L));
             int right = s_elem * S(Periodic(i,L),Periodic(j+1,L));
@@ -79,27 +46,68 @@ int Lattice::E(mat S){
     return E;
 }
 
-// Calculate Total Energy of all possible states
-int Lattice::E_all(){
-    int size = all_S.size();
-    int all = 0;
-    for (int i=0; i<size-1; i++){
-        all += E(all_S[i]);
-    }
-    return all;
+// // Calculate Total Energy of all possible states
+// int Lattice::E_all(){
+//     int size = all_S.size();
+//     int all = 0;
+//     for (int i=0; i<size-1; i++){
+//         all += E(all_S[i]);
+//     }
+//     return all;
+// }
+
+// // Partition Function Calculation
+// void Lattice::Z(){
+//     int size = all_S.size();
+//     Z_all = 0;
+//     for (int i=0; i<size; i++){
+//         Z_all += exp(beta_*E(all_S[i]));
+//     }
+//     Z_ = Z_all;
+// }
+
+// Probability Function for a given state S
+double Lattice::p(mat S){
+    return ( (1/Z_)*exp(beta_*E(S)) );
 }
 
 
-// // Calculate Magnetization in a Spin Configuration
-// int M(mat S){
-//     return S;
+
+////////##################IDK#####################////////
+
+// // Generate 1 of 16 possible states of 2x2 System
+// mat Lattice::S2(int a, int b, int c, int d){
+//     if(a == 0){ 
+//         a = -1;
+//     }
+//     if(b == 0){
+//         b = -1;
+//     }
+//     if(c == 0){
+//         c = -1;
+//     }
+//     if(d == 0){
+//         d = -1;
+//     }
+//     arma::mat A = arma::mat(2,2);
+//     A(0,0) = a;
+//     A(0,1) = b;
+//     A(1,0) = c;
+//     A(1,1) = d;
+//     return A;
 // }
-//////////
 
-
-// double P_MS(double Energy);
-// double P_MS(double Energy){ //Probablity for microstate.
-//     int beta = 1;
-//     double pms = exp(-beta * Energy) /(12 + 4 * cosh(8 * beta));
-//     return pms;
+// // Generate all 16 possible states of 2x2 System
+// void Lattice::S2_all(){
+//     for (int a = 0; a<2; a++){
+//         for (int b = 0; b<2; b++){
+//             for (int c = 0; c<2; c++){
+//                 for (int d = 0; d<2; d++){
+//                     mat S_ = S2(a,b,c,d);
+//                     all_S.push_back(S_);
+//                 }
+//             }
+//         }
+//     }
+    
 // }
